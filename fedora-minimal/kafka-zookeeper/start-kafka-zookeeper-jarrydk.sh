98a21d5b39a7b8ebd81abbdca0e0946558d60206
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2023-07-07 09:21:18 +0200

    Update Kafka

--- fedora-minimal/kafka-zookeeper/start-kafka-zookeeper-jarrydk.sh
+++ fedora-minimal/kafka-zookeeper/start-kafka-zookeeper-jarrydk.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
+KAFKA_DATA_DIR_DEFAULT='/tmp/zookeeper'
+KAFKA_ADMIN_ENABLE_SERVER_DEFAULT='false'
+KAFKA_ADMIN_SERVER_PORT_DEFAULT='8080'
+
+echo "-----------------------------------------------------------------------------------"
+echo "- KAFKA_DATA_DIR                      : $KAFKA_DATA_DIR"
+echo "--- KAFKA_DATA_DIR_DEFAULT            : $KAFKA_DATA_DIR_DEFAULT"
+echo "- KAFKA_ADMIN_ENABLE_SERVER           : $KAFKA_ADMIN_ENABLE_SERVER"
+echo "--- KAFKA_ADMIN_ENABLE_SERVER_DEFAULT : $KAFKA_ADMIN_ENABLE_SERVER_DEFAULT"
+echo "- KAFKA_ADMIN_SERVER_PORT             : $KAFKA_ADMIN_SERVER_PORT"
+echo "--- KAFKA_ADMIN_SERVER_PORT_DEFAULT   : $KAFKA_ADMIN_SERVER_PORT_DEFAULT"
+echo "-----------------------------------------------------------------------------------"
+
+
+if [ ! -z "$KAFKA_DATA_DIR" ]; then
+    sed -i 's/dataDir/# dataDir/g' zookeeper.properties
+    sed -i '17 i dataDir='$KAFKA_DATA_DIR'' /opt/kafka/config/zookeeper.properties
+else
+    sed -i '17 i # Default value is used for dataDir ('$KAFKA_DATA_DIR_DEFAULT')' /opt/kafka/config/zookeeper.properties
+fi
+
+if [ ! -z "$KAFKA_ADMIN_ENABLE_SERVER" ]; then
+    sed -i 's/admin.enableServer/# admin.enableServer/g' /opt/kafka/config/zookeeper.properties
+    sed -i '25 i admin.enableServer='$KAFKA_ADMIN_ENABLE_SERVER'' /opt/kafka/config/zookeeper.properties
+else
+    sed -i '25 i # Default value is used for admin.enableServer ('$KAFKA_ADMIN_ENABLE_SERVER_DEFAULT')' /opt/kafka/config/zookeeper.properties
+fi
+
+echo "" >> /opt/kafka/config/zookeeper.properties
+if [ ! -z "$KAFKA_ADMIN_SERVER_PORT" ]; then
+    echo "admin.serverPort=$KAFKA_ADMIN_SERVER_PORT" >> /opt/kafka/config/zookeeper.properties
+else
+    echo "# Default value is used for admin.serverPort ($KAFKA_ADMIN_SERVER_PORT_DEFAULT)" >> /opt/kafka/config/zookeeper.properties
+fi
+
 /opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties
--- fedora-minimal/kafka/start-kafka-jarrydk.sh
+++ fedora-minimal/kafka/start-kafka-jarrydk.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-echo "---------------------------"
-echo "- KAFKA_ZOOKEEPER_CONNECT : $KAFKA_ZOOKEEPER_CONNECT"
-echo "- KAFKA_LISTENERS : $KAFKA_LISTENERS"
-echo "- KAFKA_ADVERTISED_LISTENERS : $KAFKA_ADVERTISED_LISTENERS"
-echo "- KAFKA_BROKER_ID : $KAFKA_BROKER_ID"
-echo "---------------------------"
+KAFKA_LISTENERS_DEFAULT="PLAINTEXT://:9092"
+KAFKA_ADVERTISED_LISTENERS_DEFAULT="PLAINTEXT://localhost:9092"
+KAFKA_BROKER_ID_DEFAULT="0"
+KAFKA_LOG_DIRS_DEFAULT="/tmp/kafka-logs"
+
+echo "-----------------------------------------------------------------------------------"
+echo "- KAFKA_ZOOKEEPER_CONNECT                 : $KAFKA_ZOOKEEPER_CONNECT"
+echo "- KAFKA_LISTENERS                         : $KAFKA_LISTENERS"
+echo "--- KAFKA_LISTENERS_DEFAULT               : $KAFKA_LISTENERS_DEFAULT"
+echo "- KAFKA_ADVERTISED_LISTENERS              : $KAFKA_ADVERTISED_LISTENERS"
+echo "--- KAFKA_ADVERTISED_LISTENERS_DEFAULT    : $KAFKA_ADVERTISED_LISTENERS"
+echo "- KAFKA_BROKER_ID                         : $KAFKA_BROKER_ID"
+echo "--- KAFKA_BROKER_ID_DEFAULT               : $KAFKA_BROKER_ID_DEFAULT"
+echo "- KAFKA_LOG_DIRS                          : $KAFKA_LOG_DIRS"
+echo "--- KAFKA_LOG_DIRS_DEFAULT                : $KAFKA_LOG_DIRS_DEFAULT"
+echo "-----------------------------------------------------------------------------------"
 
 if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
     echo "ERROR: missing mandatory config: KAFKA_ZOOKEEPER_CONNECT"
@@ -13,20 +23,25 @@ if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
 fi
 
 if [[ -z "$KAFKA_LISTENERS" ]]; then
-    KAFKA_LISTENERS="PLAINTEXT://:9092"
+    KAFKA_LISTENERS=$KAFKA_LISTENERS_DEFAULT
 fi
 
 # https://www.confluent.io/blog/kafka-listeners-explained/
 if [[ -z "$KAFKA_ADVERTISED_LISTENERS" ]]; then
-    KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://localhost:9092"
+    KAFKA_ADVERTISED_LISTENERS=$KAFKA_ADVERTISED_LISTENERS_DEFAULT
 fi
 
 if [[ -z "$KAFKA_BROKER_ID" ]]; then
-    KAFKA_BROKER_ID="0"
+    KAFKA_BROKER_ID=$KAFKA_BROKER_ID_DEFAULT
+fi
+
+if [[ -z "$KAFKA_LOG_DIRS" ]]; then
+    KAFKA_LOG_DIRS=$KAFKA_LOG_DIRS_DEFAULT
 fi
 
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties \
     --override zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT \
     --override listeners=$KAFKA_LISTENERS \
     --override advertised.listeners=$KAFKA_ADVERTISED_LISTENERS \
-    --override broker.id=$KAFKA_BROKER_ID
+    --override broker.id=$KAFKA_BROKER_ID \
+    --override log.dirs=$KAFKA_LOG_DIRS
